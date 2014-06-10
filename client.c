ce323e5a3f7fc8bf60f760c15e4cc384dfca9eb5
Stuart Van Campen <scampen@oanda.com>
2014-06-10 15:47:08 -0400

    updates to client.c

--- client.c
+++ client.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
     sockfd = setup();
 
     //insert user
-    bzero(buffer,256);
+    memset(buffer,0,256);
     sprintf(buffer, "command=set:name=John:location=Victoria:age=20");
     //fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
          error("ERROR writing to socket");
-    bzero(buffer,256);
+    memset(buffer,0,256);
     n = read(sockfd,buffer,255);
     if (n < 0) 
          error("ERROR reading from socket");
@@ -37,13 +37,13 @@ int main(int argc, char *argv[])
 
     //get user
     sockfd = setup();
-    bzero(buffer,256);
+    memset(buffer,0,256);
     sprintf(buffer, "command=get:name=John");
     //fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
          error("ERROR writing to socket");
-    bzero(buffer,256);
+    memset(buffer,0,256);
     n = read(sockfd,buffer,255);
     if (n < 0) 
          error("ERROR reading from socket");
@@ -52,13 +52,13 @@ int main(int argc, char *argv[])
 
     //delete user
     sockfd = setup();
-    bzero(buffer,256);
+    memset(buffer,0,256);
     sprintf(buffer, "command=del:name=John");
     //fgets(buffer,255,stdin);
     n = write(sockfd,buffer,strlen(buffer));
     if (n < 0) 
          error("ERROR writing to socket");
-    bzero(buffer,256);
+    memset(buffer,0,256);
     n = read(sockfd,buffer,255);
     if (n < 0) 
          error("ERROR reading from socket");
@@ -80,11 +80,11 @@ int setup() {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    memcpy((char *)&serv_addr.sin_addr.s_addr,
+          (char *)server->h_addr, 
+          server->h_length);
     serv_addr.sin_port = htons(50000);
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
